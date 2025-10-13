93e70792eddc96f71e539fbda57306bed2eed5a4
Kelvin Mwendwa <kelvinmwendwa290505@gmail.com>
2025-10-13 16:45:16 +0300

    First script started

--- scripting/bash/lowlevel/dir-cleaner.sh
+++ scripting/bash/lowlevel/dir-cleaner.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# My first ever bash project! Very excited, hopeful I'll learn quite a bit over the course of this.
-# This is a directory cleaner, that organizes files in a folder by type and finds and replaces whitespaces
+# My first ever bash project! Very excited! Hopefully I'll learn quite a bit over the course of this.
+# This is a directory cleaner that organizes files in a folder by type and finds and replaces whitespaces
 # in filenames.
 
-DIR="."
-
-# check if files exist in the current directory
+# loop through the list of files and directories
 for filename in $(ls); do
-	if [[ "$filename" == *.txt ]]; then
+	# check for specific file extensions and move them to relevant Directories
+	if [[ "$filename" == *.txt ]] || [[ "$filename" == *.pdf ]] || [[ "$filename" == *.odt ]]; then
 		mv $filename ~/Documents/
+	elif [[ "$filename" == *.png ]] || [[ "$filename" == *.jpeg ]]; then
+		mv $filename ~/Pictures/
+	else
+		mv $filename ~/Videos/
 	fi
 done
