59e7a28a1e6d2ffda15c6ec93b5c812ee1da9857
Kelvin Mwendwa <kelvinmwendwa290505@gmail.com>
2025-10-14 11:31:04 +0300

    Second script post-review

--- scripting/bash/lowlevel/backup.sh
+++ scripting/bash/lowlevel/backup.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
+# This is v2.0
 # Second script in this series. It compresses a directory into a .tar.gz archive with a timestamp to its name.
 read -p "Enter the complete name of the directory you want to compress: " DIR;
 
-# check if directory exists
-if [[ -d "$DIR" ]]; then
-	# create backup directory
-	mkdir ~/Backups
+# current dir
+PWD=$(pwd)
 
-	# remove trailing "/"
-	CDIR=$(echo $DIR | tr -d '/')
+# clean $DIR for navigation
+CLNDIR="${DIR/'~'/$HOME}"
+
+# check if directory exists
+if [[ -d "$CLNDIR" ]]; then
+	# create backup directory
+	mkdir -p  ~/Backups
+
+	# navigate to target's parent
+	echo "Navigating to the parent directory of the target..."
+	cd $CLNDIR/..
+	echo "$(pwd)"
+
+	# remove leading "/" and "~"
+	CDIR="${DIR#'~'}"
+	CDIR="${CDIR#/}"
 
 	# create the archive
-	tar -czvf ~/Backups/$CDIR-$(date +%F).tar.gz $DIR
+	# use basename
+	base=$(basename "$CDIR")
+	tar -czf ~/Backups/${base}_backup_$(date +%F).tar.gz "$CLNDIR"
 
 	# feedback
-	echo "Backup for $DIR created at ~/Backups"
+	echo "Backup created: ~/Backups/${base}_backup_$(date +%F).tar.gz"
+
+	# navigate back to the working directory
+	echo "Navigating back to your working dir..."
+	cd $PWD
 else
 	echo "Directory "$DIR" does not exist. Enter a valid directory."
-	exit 0
+	exit 1
 fi
 
 ## NOTES ##
 # v1.0
 # I've struggled with enabling the user to specify a path to the dir instead of having to run this script
 # in the directory where the target is located.
+#
+# ChatGPT gave v1.0 a 8.5, Gemini gave it a 7. Not Bad.
+
+## CORRECTIONS AND IMPROVEMENTS ##
+# v2.0
+# 1. mdir should use -p to suppress Error thrown
+# 2. use parameter expansion to remove a single leading forwardslash, not all of them. ${DIR#/}. ${DIR%/} ->
+# removes trailing.
+# 3. Use basename instead of $CDIR in the name
