9374a8673f488e0bf16564a094696c0b9ceec3c7
Kelvin Mwendwa <kelvinmwendwa290505@gmail.com>
2025-10-15 22:24:08 +0300

    Script three post-review

--- scripting/bash/lowlevel/sysinfo.sh
+++ scripting/bash/lowlevel/sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v1.0
+# v2.0
 # Script number 3 in my lowlevel series
 # This script prints a neat summary of system status
 # echo -e "\e[31mThis text is red\e[0m" -> print colorful output using ANSI escape codes.
@@ -15,11 +15,10 @@ RESET='\e[0m'
 UPTIME=$(uptime -p)
 USAGE=$(df -h / | awk 'NR==2 {print $5}')
 MEM=$(free | awk '/Mem/{print int($3/$2 * 100)}')
+LOAD=$(awk '{print $1}' /proc/loadavg)
 
 # Check connectivity
-ping -c4 8.8.8.8 &> 1
-
-if [[ $? -eq 0 ]]; then
+if ping -c4 8.8.8.8 &> /dev/null; then
 	STATUS=online
 else
 	STATUS=offline
@@ -27,6 +26,7 @@ fi
 
 # The actual output
 echo -e "${GREEN}====== SYSTEM STATUS ======${RESET}"
+echo -e "Time: ${BLUE}$(date)${RESET}"
 echo -e "User: ${GREEN}$(whoami)${RESET}"
 echo -e "Hostname: ${GREEN}$(hostname)${RESET}"
 echo -e "Uptime: ${GREEN}${UPTIME#'up '}${RESET}"
@@ -40,6 +40,8 @@ elif [[ ${MEM} -ge 50 ]]; then
 else
 	echo -e "Memory: ${GREEN}${MEM}% (Low)${RESET}"
 fi
+# CPU load average
+echo -e "CPU Load: ${GREEN}${LOAD}${RESET}"
 
 # Extra flair for internet status
 if [[ ${STATUS} = online ]]; then
@@ -49,3 +51,14 @@ else
 fi
 
 echo -e "${GREEN}===========================${RESET}"
+
+### CORRECTIONS AND IMPROVEMENTS ###
+# v1.0 got a rating of 8.7 from chatGPT and an 8 from Gemini :)
+# 1. Instead of redirecting ping to a file named 1, redirect to /dev/null.
+# 2. Replace use of $?(though effective), with an if clause, which is safer. $? could
+# be overwritten by a background process.
+# 3. Add CPU load average for an even cooler script :)
+# 4. Add date and time for context.
+
+## Some of the recommended improvements were to make the script more compatible with
+# other systems. I didn't see the need since this will only be ran on mine predominantly.
