a745593ff97179323cbc6c37138a6ac6051fe4ff
Kelvin Mwendwa <kelvinmwendwa290505@gmail.com>
2026-02-04 10:52:51 +0300

    code cleanup

--- scripting/bash/lowlevel/cleaner.sh
+++ scripting/bash/lowlevel/cleaner.sh
@@ -8,23 +8,26 @@ BLUE='\e[34m'
 RESET='\e[0m'
 
 # Use this to clean when running out of space
-echo ""
-echo -e "${GREEN}Checking what's hogging space...output in a few...${RESET}"
-echo ""
-echo -e "${YELLOW}---------- Top 20 hoggers -----------${RESET}"
-echo ""
+echo -e "\n${GREEN}Checking what's hogging space...output in a few...${RESET}\n"
+
+echo -e "${YELLOW}---------- Top 20 hoggers -----------${RESET}\n"
+
 sudo du -xh / 2>/dev/null | sort -h | tail -n 20
+
 echo ""
-echo -e "${YELLOW}-------------------------------------${RESET}"
-echo ""
-echo -e "${GREEN}Doing safe cleaning...${RESET}"
-echo ""
-echo -e "${GREEN}Chrome no longer exists :)...${RESET}"
+
+echo -e "${YELLOW}-------------------------------------${RESET}\n"
+
+echo -e "${GREEN}Doing safe cleaning...${RESET}\n"
+
+echo -e "${GREEN}Chrome no longer exists :)...${RESET}\n"
+
 rm -rf ~/.cache/google-chrome/*
-echo ""
-echo -e "${GREEN}Clearing spotify cache...${RESET}"
+
+echo -e "${GREEN}Clearing spotify cache...${RESET}\n"
+
 rm -rf ~/.cache/spotify/*
-echo ""
-echo -e "${YELLOW}[*] Successfully cleared! Have a look at all your new space! :)${RESET}"
-echo ""
+
+echo -e "${YELLOW}[*] Successfully cleared! Have a look at all your new space! :)${RESET}\n"
+
 df -h
